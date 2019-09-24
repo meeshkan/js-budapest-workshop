07964b9d086b0a9491c0b0c40937ed2fba3318d2
Mike Solomon <mike@meeshkan.com>
2019-09-24 11:23:11 +0200

    It works, I'm asking my boss for a raise.

--- test/mikes-awesome-function.test.ts
+++ test/mikes-awesome-function.test.ts
@@ -1,4 +1,14 @@
 import fetchAttendees from "../src/mikes-awesome-function";
+import unmock, { u } from "unmock";
+import { IService } from "unmock-core/dist/service/interfaces";
+
+unmock
+  .nock("https://www.js-budapest.com/api")
+  .get("/attendees")
+  .reply(200, { attendees: [{ id: 0, name: "Foo McBar" }]});
+
+beforeAll(() => unmock.on());
+afterAll(() => unmock.off());
 
 test("mikes awesome function in fact adds two numbers", async () => {
   const attendees = await fetchAttendees();
