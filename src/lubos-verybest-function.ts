963b8a12daed214fc301211c1916795bf83fdaad
Luboš Hradecký <hradecky@cngroup.dk>
2019-09-24 11:55:28 +0200

    Fetching person

--- src/lubos-verybest-function.ts
+++ src/lubos-verybest-function.ts
@@ -4,3 +4,8 @@ export default async () => {
   const { data } = await axios("https://www.js-budapest.com/api/somedata");
   return data.somedata;
 }
+
+export const getPerson = async (id: number) => {
+  const { data } = await axios(`https://www.js-budapest.com/api/persons/${id}`);
+  return data;
+}
