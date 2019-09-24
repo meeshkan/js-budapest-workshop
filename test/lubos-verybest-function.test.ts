1fc54fe798ffb0501fb4708ff114fa8cf5e66713
Luboš Hradecký <hradecky@cngroup.dk>
2019-09-24 12:32:31 +0200

    Tests for dynamic data

--- test/lubos-verybest-function.test.ts
+++ test/lubos-verybest-function.test.ts
@@ -1,23 +1,28 @@
 import fetchSomeData, { getPerson } from "../src/lubos-verybest-function";
-import unmock from "unmock";
+import unmock, { u } from "unmock";
 
 unmock
   .nock("https://www.js-budapest.com/api")
   .get("/somedata")
-  .reply(200, { somedata: [{ id: 0, data: "Awesome data" }]})
+  .reply(200, { somedata: u.array({
+    id: u.number(),
+    data: u.object(u.object())
+  })})
   .get("/persons/{id}")
-  .reply(200, { name: "Jack", surname: "Handsome" });
+  .reply(200, { name: u.string(), lastname: u.string('name.lastName') });
 
 beforeAll(() => unmock.on());
 afterAll(() => unmock.off());
 
 test("should fetch some data", async () => {
   const somedata = await fetchSomeData();
-  expect(somedata.length).toBeGreaterThan(0);
+  expect(somedata instanceof Array).toBe(true);
+  expect(typeof somedata[0].id).toBe("number");
+  expect(typeof somedata[0].data).toBe("object");
 });
 
 test("gets person", async () => {
   const person = await getPerson(2);
-  expect(person.name).toBe("Jack");
-  expect(person.surname).toBe("Handsome");
+  expect(typeof person.name).toBe("string");
+  expect(typeof person.lastname).toBe("string");
 });
