2799439d1872b435d78cd9736a44f0e08bd26ab7
Jan Labik <labik@cngroup.dk>
2019-09-24 11:16:43 +0200

    coffee test

--- src/honza-function.ts
+++ src/honza-function.ts
@@ -1,1 +1,10 @@
+<<<<<<< Updated upstream
 export default (a: number, b: number) => a * b;
+=======
+import axios from 'axios';
+
+export default async() => {
+    const { data } = await axios("https://mylittle-api.com/coffees");
+    return data.coffees;
+};
+>>>>>>> Stashed changes
--- test/honzas-function.test.ts
+++ test/honzas-function.test.ts
@@ -1,5 +1,13 @@
+<<<<<<< Updated upstream
 import multiply from "../src/honza-function";
 
 test("honzas multiply function", () => {
     expect(multiply(3, 5)).toBe(15);
+=======
+import fetchCoffees from "../src/honza-function";
+
+test ("testing honzas API", async() => {
+    const coffees = await fetchCoffees();
+    expect(coffees.length).toBeGreaterThanOrEqual(100000);
+>>>>>>> Stashed changes
 });
