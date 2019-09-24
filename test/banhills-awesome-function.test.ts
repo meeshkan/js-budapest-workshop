8de6954bb18617846b2a84cee5519bd4368f9da6
Gábor Bánhegyesi <gbanhegyesi@whitepages.com>
2019-09-24 11:29:19 +0200

    Use unmock in test

--- test/banhills-awesome-function.test.ts
+++ test/banhills-awesome-function.test.ts
@@ -1,4 +1,13 @@
 import fetchLobotomies from "../src/banhills-awesome-function";
+import unmock from "unmock";
+
+unmock
+  .nock("https://www.js-budapest.com/api")
+  .get("/lobotomies")
+  .reply(200, { lobotomies: [{ id: 0, name: 'Some Unfortunate Guy' }] });
+
+beforeAll(() => unmock.on());
+afterAll(() => unmock.off());
 
 test("banhills awesome function pulls something from an api", async () => {
   const lobotomies = await fetchLobotomies();
