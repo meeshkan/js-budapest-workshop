44ead87abf17e759c54f7368eadbe3151868ae1b
Francisco Perestrelo <francisco.perestrelo@pst.asseco.com>
2019-09-24 10:10:44 +0100

    Add test in error

--- test/fnsp-awesome-function.test.ts
+++ test/fnsp-awesome-function.test.ts
@@ -2,4 +2,5 @@ import divide from "../src/fnsp-awesome-function";
 
 test("fnsp awesome function in fact divide two numbers", () => {
   expect(divide(6,2)).toBe(3);
+  expect(divide(6,0)).toBe(0);
 });
