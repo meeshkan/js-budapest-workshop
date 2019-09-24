53a735cae8b57c431801ccf35fc523196f8261d5
Jan Labik <labik@cngroup.dk>
2019-09-24 11:19:38 +0200

    fix

--- src/honza-function.ts
+++ src/honza-function.ts
@@ -1,10 +1,6 @@
-<<<<<<< Updated upstream
-export default (a: number, b: number) => a * b;
-=======
-import axios from 'axios';
+
 
 export default async() => {
     const { data } = await axios("https://mylittle-api.com/coffees");
     return data.coffees;
 };
->>>>>>> Stashed changes
