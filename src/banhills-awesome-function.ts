8eb1f1e4d0db6b0af7521d2dff12283c22c9edeb
Gábor Bánhegyesi <gbanhegyesi@whitepages.com>
2019-09-24 11:47:17 +0200

    Implement individual lobotomy function

--- src/banhills-awesome-function.ts
+++ src/banhills-awesome-function.ts
@@ -4,3 +4,8 @@ export default async () => {
   const { data } = await axios("https://www.js-budapest.com/api/lobotomies");
   return data.lobotomies;
 }
+
+export const getIndividualLobotomy = async (id: number) => {
+  const { data } = await axios("https://www.js-budapest.com/api/lobotomies/${id}");
+  return data.lobotomies;
+}
