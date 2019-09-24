c63f06ebed2d3803ac8dfa2c8dfc941794d5929c
Jepser Bernardino <jepsersk8@gmail.com>
2019-09-24 12:18:11 +0200

    test: use dynamic results

--- test/jepser-awesome-function.test.ts
+++ test/jepser-awesome-function.test.ts
@@ -1,4 +1,4 @@
-import unmock, { transform } from 'unmock'
+import unmock, { transform, u } from 'unmock'
 import { fetchAttendees, fetchAttendee, API_BASE_URL, API_ATTENDEES_PATH } from '../src/jepser-awesome-function'
 import { IService } from 'unmock-core/dist/service/interfaces';
 
@@ -7,16 +7,16 @@ const { responseBody } = transform;
 unmock
   .nock(API_BASE_URL, "conference")
   .get(API_ATTENDEES_PATH)
-  .reply(200, [
+  .reply(200, u.array(
     {
-      id: 1,
-      name: 'batman'
+      id: u.integer({ minimum: 0}),
+      name: u.string('name.firstName')
     }
-  ])
+  ))
   .get(`${API_ATTENDEES_PATH}/{id}`)
   .reply(200, {
-      id: 1,
-      name: 'batman'
+      id: u.integer({ minimum: 0}),
+      name: u.string('name.firstName')
     })
 
 let conference: IService;
@@ -28,22 +28,20 @@ afterAll(() => unmock.off())
 describe('fetchAttendees', () => {
   it('should return the list of attendees', async () => {
     const response = await fetchAttendees()
-    expect(response).toHaveLength(1)
+    expect(response instanceof Array).toBeTruthy()
     expect(response[0]).toEqual(expect.objectContaining({
-      id: 1,
-      name: 'batman'
+      id: expect.any(Number),
+      name: expect.any(String)
     }))
   })
 })
 
 describe('fetchAttendee', () => {
   it('should return the correct attendee for the given id', async () => {
-    conference.state((req, o) => responseBody({path: "/attendees/{id}", lens: ["id"]}).const(+req.pathname.split("/").slice(-1))(req, o));
-    const myNum = 42;
-    const response = await fetchAttendee(myNum)
+    const response = await fetchAttendee(1)
     expect(response).toEqual(expect.objectContaining({
-      id: 1,
-      name: 'batman'
+      id: expect.any(Number),
+      name: expect.any(String)
     }))
   })
 })
