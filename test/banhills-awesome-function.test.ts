25352d58e41c4523948b636256a87b8b53be2123
Gábor Bánhegyesi <gbanhegyesi@whitepages.com>
2019-09-24 11:13:46 +0200

    Call nonexistent api

--- test/banhills-awesome-function.test.ts
+++ test/banhills-awesome-function.test.ts
@@ -1,5 +1,6 @@
-import adder from "../src/banhills-awesome-function";
+import fetchAttendees from "../src/banhills-awesome-function";
 
-test("banhills awesome function in fact adds two numbers", () => {
-  expect(adder(2, 5)).toBe(7);
+test("banhills awesome function pulls something from an api", async () => {
+  const attendees = await fetchAttendees();
+  expect(attendees.length).toBeGreaterThan(0);
 });
