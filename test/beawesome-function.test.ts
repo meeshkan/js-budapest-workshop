6b826daf65d913a8ad2de088f6c62045a316f68b
Huszar Beata <beata.huszar@adevinta.com>
2019-09-24 12:33:41 +0200

    Add one more example

--- test/beawesome-function.test.ts
+++ test/beawesome-function.test.ts
@@ -6,7 +6,7 @@ import { string } from "io-ts";
 unmock
     .nock("https://www.js-budapest.com/api")
     .get("/attendees")
-    .reply(200, { attendees: u.array({ id: u.integer(), name: u.string() }) })
+    .reply(200, { attendees: u.array({ id: u.integer({ minimum: 1 }), name: u.string() }) })
     .get("/attendees/{id}")
     .reply(200, { id: u.integer(), name: u.string() });
 
@@ -20,6 +20,7 @@ test("it should fetch dem attendees", async () => {
 
 test("get attendee by id", async () => {
     const attendee = await getAttendeeByID(4);
+    expect(attendee.id).toBeGreaterThan(0);
     expect(typeof attendee.name).toBe("string");
 });
 
