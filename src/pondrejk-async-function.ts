b5c0b066e17962678f97195fb89c83684bffebd0
peter.ondrejka@oracle.com <peter.ondrejka@oracle.com>
2019-09-24 15:21:03 +0200

    Adding transform

--- src/pondrejk-async-function.ts
+++ src/pondrejk-async-function.ts
@@ -2,12 +2,22 @@ import axios from "axios";
 
 export async function fetchInitiatives() {
     //const response: any [] = await axios.get("http://its-a-green-world.com/initiatives");
-    const { data } = await axios.get("http://its-a-green-world.com/initiatives");
-    return {
-        ...data,
-        onInternetExplorer: true,
-        timestamp: new Date().getTime()
-    };
+    try {
+        const { data } = await axios.get("http://its-a-green-world.com/initiatives");
+        return {
+            ...data,
+            onInternetExplorer: true,
+            error: false,
+            timestamp: new Date().getTime()
+        };
+    } catch (e) {
+        return {          
+          initiatives: [],
+          onInternetExplorer: true,
+          error: true,
+          timestamp: new Date().getTime()
+        }
+    }   
 }
 
 export async function fetchIndividualInitiative(id: string) {
--- test/pondrejk-async-function.test.ts
+++ test/pondrejk-async-function.test.ts
@@ -1,16 +1,19 @@
 import { fetchInitiatives, fetchIndividualInitiative } from "../src/pondrejk-async-function";
-import unmock, {u} from "unmock";
+import unmock, {u, runner, transform} from "unmock";
 import { IService } from "unmock-core/dist/service/interfaces";
+const { withCodes, responseBody } = transform;
 
 unmock
   .nock("http://its-a-green-world.com", "greenWorld")
   .get("/initiatives")
   .reply(200, { initiatives: u.array({id: u.string(), name: u.string()})})
+  .reply(401, { message: "Unauthorized" })
   .get("/initiatives/{id}")
   .reply(200, {
     id: u.string(),
     name: u.string()   
-  });
+  })
+  .reply(401, { message: "Unauthorized" });
 
 unmock
   .nock("http://my-analytics.com/api", "analytics")
@@ -30,17 +33,35 @@ afterEach(() => {
   }
 );
 
-test("testing async function", async () => {
+test("testing async function when it works", runner(
+    async () => {        
+        greenWorld.spy.resetHistory();
+        greenWorld.state(withCodes(200));
+        const initiatives = await fetchInitiatives();
+        //console.log(initiatives);
+        expect(initiatives.initiatives instanceof Array).toBe(true);
+        expect(initiatives).toMatchObject({
+            ...JSON.parse(greenWorld.spy.getResponseBody()),
+            onInternetExplorer: true
+        });
+        console.log(initiatives.initiatives.length);
+        if (initiatives.initiatives.length > 5) {
+            expect(typeof initiatives.initiatives[3].id).toBe("string");
+        }
+        expect(initiatives.error).toBe(false);
+        greenWorld.spy.resetHistory();
+        const initiative1000 = await fetchIndividualInitiative("1000");
+        expect(typeof initiative1000.name).toBe("string");
+        expect(initiative1000).toMatchObject({
+            ...JSON.parse(greenWorld.spy.getResponseBody())
+        })
+    })
+);
+
+test("testing async function it does not work", runner(async () => {
+    greenWorld.state(withCodes(401))
     const initiatives = await fetchInitiatives();
     expect(initiatives.initiatives instanceof Array).toBe(true);
-    expect(initiatives).toMatchObject({
-        ...JSON.parse(greenWorld.spy.getResponseBody()),
-        onInternetExplorer: true
-      });
-    greenWorld.spy.resetHistory();
-    const initiative1000 = await fetchIndividualInitiative("1000");
-    expect(typeof initiative1000.name).toBe("string");
-    expect(initiative1000).toMatchObject({
-        ...JSON.parse(greenWorld.spy.getResponseBody())
-    })
-});
+    expect(initiatives.initiatives.length).toBe(0);
+    expect(initiatives.error).toBe(true);
+  }));
