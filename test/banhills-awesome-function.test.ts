cd0b3d59e808d33237b9245f03db6480f0bfac3e
Gábor Bánhegyesi <gbanhegyesi@whitepages.com>
2019-09-24 13:33:15 +0200

    Expect response body with spy, match object

--- test/banhills-awesome-function.test.ts
+++ test/banhills-awesome-function.test.ts
@@ -1,19 +1,32 @@
 import fetchLobotomies, { getIndividualLobotomy } from "../src/banhills-awesome-function";
 import unmock, { u } from "unmock";
+import { IService } from "unmock-core/dist/service/interfaces"
+// import * as jsp from "json-schema-poet";
 
 unmock
-  .nock("https://www.js-budapest.com/api")
+  .nock("https://www.js-budapest.com/api", "budapest")
   .get("/lobotomies")
   .reply(200, { lobotomies: u.array({ id: u.integer(), name: u.string() }) })
   .get("/lobotomies/{id}")
   .reply(200, { id: u.integer(), name: u.string() });
 
-beforeAll(() => unmock.on());
+let budapest: IService;
+
+beforeAll(() => {
+  const services = unmock.on().services;
+  budapest = services.budapest;
+});
+beforeEach(() => {
+  budapest.reset()
+})
 afterAll(() => unmock.off());
 
 test("banhills awesome function pulls something from an api", async () => {
   const lobotomies = await fetchLobotomies();
-  expect(lobotomies instanceof Array).toBe(true);
+  expect(lobotomies).toMatchObject({
+    ...JSON.parse(budapest.spy.getResponseBody()),
+    onInternetExplorer: true
+  })
 });
 
 test("get individual lobotony", async () => {
