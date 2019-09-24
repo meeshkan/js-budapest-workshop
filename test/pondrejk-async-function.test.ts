e6b5ec274de7dba3399d597c0385cc716fe021fb
peter.ondrejka@oracle.com <peter.ondrejka@oracle.com>
2019-09-24 11:49:09 +0200

    Fixing the async test with unmock

--- test/pondrejk-async-function.test.ts
+++ test/pondrejk-async-function.test.ts
@@ -1,4 +1,16 @@
 import { fetchInitiatives } from "../src/pondrejk-async-function";
+import unmock from "unmock";
+
+unmock
+  .nock("http://its-a-green-world.com")
+  .get("/initiatives")
+  .reply(200, { initiatives: [{
+    id: "1000",
+    name: "Plant Trees"    
+  }]})
+
+beforeAll(() => unmock.on());
+afterAll(() => unmock.off());  
 
 test("testing async function", async () => {
     const initiatives = await fetchInitiatives();
