6e46cddf8e8397f75ade3dce9adf3e287318551d
Jepser Bernardino <jepsersk8@gmail.com>
2019-09-24 11:08:39 +0200

    feat(api): create get attendees getter

--- src/jepser-awesome-function.ts
+++ src/jepser-awesome-function.ts
@@ -1,1 +1,9 @@
-export default (a: number, b: number) => a - b;
+import axios from 'axios'
+
+const fetchAttendees = async () => {
+  const { data } = await axios.get('https://superapi.com/attendees')
+
+  return data
+}
+
+export default fetchAttendees
--- test/jepser-awesome-function.test.ts
+++ test/jepser-awesome-function.test.ts
@@ -1,7 +1,8 @@
-import minus from '../src/jepser-awesome-function'
+import fetchAttendees from '../src/jepser-awesome-function'
 
-describe('minus', () => {
-  it('should substrat the second number to the first', () => {
-    expect(minus(3, 1)).toBe(2)
+describe('fetchAttendees', () => {
+  it('should return the list of attendees', async () => {
+    const response = await fetchAttendees()
+    expect(response).toBe([])
   })
 })
