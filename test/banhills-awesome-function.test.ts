dcf6846fbd5167d386637f5412298d4047946377
Gábor Bánhegyesi <gbanhegyesi@whitepages.com>
2019-09-24 11:46:59 +0200

    Test individual lobotomy get request

--- test/banhills-awesome-function.test.ts
+++ test/banhills-awesome-function.test.ts
@@ -1,9 +1,11 @@
-import fetchLobotomies from "../src/banhills-awesome-function";
+import fetchLobotomies, { getIndividualLobotomy } from "../src/banhills-awesome-function";
 import unmock from "unmock";
 
 unmock
   .nock("https://www.js-budapest.com/api")
   .get("/lobotomies")
+  .reply(200, { lobotomies: [{ id: 0, name: 'Some Unfortunate Guy' }] })
+  .get("/lobotomies/{id}")
   .reply(200, { lobotomies: [{ id: 0, name: 'Some Unfortunate Guy' }] });
 
 beforeAll(() => unmock.on());
@@ -11,5 +13,10 @@ afterAll(() => unmock.off());
 
 test("banhills awesome function pulls something from an api", async () => {
   const lobotomies = await fetchLobotomies();
+  expect(lobotomies.length).toBeGreaterThan(0);
+});
+
+test("get individual lobotony", async () => {
+  const lobotomies = await getIndividualLobotomy(9);
   expect(lobotomies.length).toBeGreaterThan(0);
 });
